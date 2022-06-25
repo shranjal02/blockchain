67f87288249dc927e198b1a630e9ce8872051df8
Shranjal <agrawalshranjal@gmail.com>
2022-06-25 18:32:45 +0530

    Blockchain - Ethereum

---
+++ blockchain.test.js
@@ -0,0 +1,24 @@
+const Blockchain = require("./blockchain");
+const Block = require("./block");
+
+describe("Blockchain", () => {
+  let bc, bc2;
+  beforeEach(() => {
+    bc = new Blockchain();
+    bc2 = new Blockchain();
+  });
+
+  it("starts with genesis block", () => {
+    expect(bc.chain[0]).toEqual(Block.genesis());
+  });
+  it("adds a new block", () => {
+    const data = "foo";
+    bc.addBlock(data);
+    expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+  });
+  it("validates a valid chain", () => {
+    bc2.addBlock("foo");
+
+    expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
+});
